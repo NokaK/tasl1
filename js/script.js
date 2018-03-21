eae30208cf6372d847ef4f5a97e5974f428e8b8e
NokaK <nona.kipiani07b@geolab.edu.ge>
2018-03-21 13:49:19 +0400

    add js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 $(function() {
+    $("#portfolio-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#portfolio").offset().top
+        }, 500);
+    });
+    $("#about-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 500);
+    });
+    $("#contact-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+    });
 
-$("#portfolio-link").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#portfolio").offset().top
-    }, 500);
-});
-$("#contact-link").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 500);
-});
-$("#about-link").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#contact").offset().top
-    }, 500);
-});
+    Query(document).ready(function() {
+        jQuery('.post').addClass("hidden").viewportChecker({
+            classToAdd: 'visible animated flipInX',
+            offset: 100
+           });
+
+    });
+    jQuery(document).ready(function() {
+        jQuery('.post1').addClass("hidden").viewportChecker({
+            classToAdd: 'visible animated bounceInUp',
+            offset: 100
+           });
+
+	});
+    jQuery(document).ready(function() {
+        jQuery('.post2').addClass("hidden").viewportChecker({
+            classToAdd: 'visible animated  flipInX',
+            offset: 100
+           });
+    })
+})
